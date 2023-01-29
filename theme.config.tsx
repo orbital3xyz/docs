ce89c4e8e138ad120aabaa8c96f87e0bd77f6e6a
Liu Pengpeng <dev@cryptoless.io>
2023-01-29 19:54:15 +0800

    init

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,23 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Orbital3</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/orbital3xyz/docs',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/sJb6gvzcgA',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/orbital3xyz/docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: <span>
+      MIT {new Date().getFullYear()} © <a href="https://orbital3.xyz" target="_blank">Orbital3</a>.
+    </span>,
   },
+  i18n: [
+    { locale: 'en', text: 'English' }, 
+    { locale: 'zh', text: '简体中文' },
+  ],
 }
 
 export default config
